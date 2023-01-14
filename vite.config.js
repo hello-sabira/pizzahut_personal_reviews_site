948c4220bb63fab833b0f6ecfe042c29114c1ade
sabira-khan <sabira.khan248@gmail.com>
2023-01-14 13:32:17 +0300

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vue-project/',
+  // base: '/deploying-vue-project/',
   plugins: [vue()],
 })
